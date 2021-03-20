4aae469f745275cc83b01aa889a1b0886cc73c77
Marco Solina <marcosolina@gmail.com>
2021-03-20 20:55:43 +0100

    Update downloadAndStartContainers.sh

--- Scripts/Docker/downloadAndStartContainers.sh
+++ Scripts/Docker/downloadAndStartContainers.sh
@@ -20,7 +20,8 @@ git clone https://github.com/marcosolina/covidstatus.git
 GIT_FOLDER="./covidstatus"
 FOLDER_TO_KEEP="$GIT_FOLDER/Scripts"
 
-find $GIT_FOLDER -mindepth 1 ! -regex '^'$FOLDER_TO_KEEP'\(/.*\)?' -delete
-find $GIT_FOLDER -maxdepth 3 -type f -name "*.sh" -delete
+# This does not work on Mac
+#find $GIT_FOLDER -mindepth 1 ! -regex '^'$FOLDER_TO_KEEP'\(/.*\)?' -delete
+#find $GIT_FOLDER -maxdepth 3 -type f -name "*.sh" -delete
 
 docker-compose -f covidstatus/Scripts/Docker/app-and-db-docker-compose.yml up
