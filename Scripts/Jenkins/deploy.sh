b631763cf5e41894d861216ce3bd9f85e347b29d
Marco Solina <marcosolina@gmail.com>
2023-05-25 21:15:49 +0200

    updates

--- Scripts/Jenkins/deploy.sh
+++ Scripts/Jenkins/deploy.sh
@@ -13,7 +13,7 @@ mvn clean package -f $WORKSPACE_FOLDER/pom.xml
 
 ssh $SSH_ADDRESS_1 mkdir -p $DEPLOY_APP_FOLDER
 
-scp $WORKSPACE_FOLDER/$APP_NAME/Scripts/startStop.sh $SSH_ADDRESS_1:$DEPLOY_APP_FOLDER
+scp $WORKSPACE_FOLDER/Scripts/startStop.sh $SSH_ADDRESS_1:$DEPLOY_APP_FOLDER
 ssh $SSH_ADDRESS_1 chmod +x $DEPLOY_APP_FOLDER/startStop.sh
 ssh $SSH_ADDRESS_1 $DEPLOY_APP_FOLDER/startStop.sh stop
 
@@ -22,7 +22,7 @@ sleep 5
 #psql -w -U postgres -f $WORK_SPACE_FOLDER/Scripts/Docker/PostgreSQL/initCovidDb.sql
 
 mv target/covidstatus*.jar $APP_FOLDER/Covidstatus.jar
-scp $WORKSPACE_FOLDER/$APP_NAME/target/$APP_NAME*.jar $SSH_ADDRESS_1:$DEPLOY_APP_FOLDER/$APP_NAME.jar
+scp $WORKSPACE_FOLDER/target/$APP_NAME*.jar $SSH_ADDRESS_1:$DEPLOY_APP_FOLDER/$APP_NAME.jar
 
 ssh -t -t $SSH_ADDRESS_1 << EOF
 export BASH_ENV=/etc/bash.bashrc && echo "" > $DEPLOY_APP_FOLDER/$APP_NAME.log && $DEPLOY_APP_FOLDER/startStop.sh start && exit
